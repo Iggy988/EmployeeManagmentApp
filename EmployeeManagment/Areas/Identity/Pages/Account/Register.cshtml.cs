058485d4d5beb8f8c0192e11dbc43e8991be75cc
Iggy988 <igor.rodic1988@gmail.com>
2023-11-28 17:02:02 +0100

    New User

--- EmployeeManagment/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EmployeeManagment/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,19 +1,35 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace EmployeeManagment.Areas.Identity.Pages.Account
-{
-    public class RegisterModel : PageModel
-    {
-        [BindProperty]
-        public InputModel Input { get; set; }
-        public void OnGet()
-        {
-        }
+namespace EmployeeManagment.Areas.Identity.Pages.Account;
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            return Page();
-        }
+public class RegisterModel : PageModel
+{
+
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public RegisterModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
     }
+
+    [BindProperty]
+    public InputModel Input { get; set; }
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (ModelState.IsValid)
+        {
+            var identity = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+            var result = await _userManager.CreateAsync(identity, Input.Password);
+        }
+        return Page();
+    }
+
 }
