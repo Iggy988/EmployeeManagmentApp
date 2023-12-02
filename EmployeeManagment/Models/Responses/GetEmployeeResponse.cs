f6a962546e793e6dedc8169428863c4dcd7d538f
Iggy988 <igor.rodic1988@gmail.com>
2023-12-02 22:29:19 +0100

    EditForm

---
+++ EmployeeManagment/Models/Responses/GetEmployeeResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace EmployeeManagment.Models.Responses;
+
+public class GetEmployeeResponse : BaseResponse
+{
+    public Employee Employee { get; set; }
+}
--- EmployeeManagment/Services/EmployeeService.cs
+++ EmployeeManagment/Services/EmployeeService.cs
@@ -10,6 +10,9 @@ public interface IEmployeeService
 {
     Task<GetEmployeesResponse> GetEmployees();
     Task<BaseResponse> AddEmployee(AddEmployeeForm form);
+    Task<GetEmployeeResponse> GetEmployee(int id);  
+    Task<BaseResponse> DeleteEmployee(Employee employee);  
+    Task<BaseResponse> EditEmployee(Employee employee);  
 }
 
 public class EmployeeService : IEmployeeService
@@ -80,6 +83,94 @@ public class EmployeeService : IEmployeeService
         {
             response.StatusCode = 500;
             response.Message = "Error adding employee: " + ex.Message;
+        }
+
+        return response;
+    }
+
+    public async Task<GetEmployeeResponse> GetEmployee(int id)
+    {
+        var response = new GetEmployeeResponse();
+
+        try
+        {
+            using (var context = _factory.CreateDbContext())
+            {
+                var employee = await  context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+                
+                response.StatusCode = 200;
+                response.Message = "Success";
+                response.Employee = employee;
+            }
+        }
+        catch (Exception ex)
+        {
+            response.StatusCode = 500;
+            response.Message = "Error retrieving employee: " + ex.Message;
+            response.Employee = null;
+        }
+
+        return response;
+    }
+
+    public async Task<BaseResponse> DeleteEmployee(Employee employee)
+    {
+        var response = new BaseResponse();
+        try
+        {
+            using (var context = _factory.CreateDbContext())
+            {
+                context.Remove(employee);
+
+                var result = await context.SaveChangesAsync();
+
+                if (result == 1)
+                {
+                    response.StatusCode = 200;
+                    response.Message = "Employee deleted successfully";
+                }
+                else
+                {
+                    response.StatusCode = 400;
+                    response.Message = "Error occured while deliting employee";
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            response.StatusCode = 500;
+            response.Message = "Error deliting employee: " + ex.Message;
+        }
+
+        return response;
+    }
+
+    public async Task<BaseResponse> EditEmployee(Employee employee)
+    {
+        var response = new BaseResponse();
+        try
+        {
+            using (var context = _factory.CreateDbContext())
+            {
+                context.Update(employee);
+                var result = await context.SaveChangesAsync();
+
+                if (result == 1)
+                {
+                    response.StatusCode = 200;
+                    response.Message = "Employee updated successfully";
+                }
+                else
+                {
+                    response.StatusCode = 400;
+                    response.Message = "Error occured while updating employee";
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            response.StatusCode = 500;
+            response.Message = "Error updating employee: " + ex.Message;
         }
 
         return response;
